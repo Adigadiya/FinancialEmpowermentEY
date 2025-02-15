43c02e5e8a9cae97754d81f4cc6647b645f08730
Aditya Gadiya <aditya@example.com>
2025-02-15 23:54:31 +0530

    Chatbot merged

---
+++ app.config.js
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: "FinancialEmpowermentApp",
+    slug: "FinancialEmpowermentApp",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      image: "./assets/splash-icon.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: { supportsTablet: true },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff"
+      }
+    },
+    web: { favicon: "./assets/favicon.png" },
+    extra: {
+      OPENAI_API_KEY: process.env.OPENAI_API_KEY
+    }
+  }
+};
